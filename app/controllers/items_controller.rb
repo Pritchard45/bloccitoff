f953ebcf11a7da3bf20f2093dd804bf54a8d86a7
Pritchard45 <alexpritchard45@gmail.com>
2019-02-20 12:21:51 -0500

    added alerts, items can be deleted with Ajax, view renders back to users proflie

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,35 @@
 class ItemsController < ApplicationController
-
+  # create an item
   def create
-    @item = current_user.items.build(item_params)
+      @item = current_user.items.build(item_params)
 
-    if @item.save
-      flash[:success] = "Item successfully saved."
-      redirect_to current_user
-    else
-      flash[:alert] = "There was an error saving your item."
-      redirect_to :back
+      if @item.save
+        flash[:success] = "Task successfully saved."
+        redirect_to current_user
+      else
+        flash[:alert] = "There was an error saving your task."
+        redirect_to :back
+    end
   end
-end
+
+  # destroy an item
+  def destroy
+      @item = Item.find(params[:id])
+
+      if @item.user == current_user
+        @item.destroy
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+
+    redirect_to current_user
+    flash[:success] = "Task was completed! You are the task master!"
+    end
+  end
+
+
 
   private
   def item_params
